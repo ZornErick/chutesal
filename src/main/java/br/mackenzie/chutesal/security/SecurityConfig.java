f20fe22beb6b9869770d06c5c7591d4b936c2d47
Erick <erick.nogueira.zorn@gmail.com>
2022-09-19 21:16:59 -0300

    DataBase password changed CorsConfiguration

--- src/main/java/br/mackenzie/chutesal/security/SecurityConfig.java
+++ src/main/java/br/mackenzie/chutesal/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.csrf().disable().cors();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/auth").permitAll()
